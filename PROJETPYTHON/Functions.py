2211a3a0509b812eb2bc804bb359f1e3cb255717
bigam128 <119424783+bigam128@users.noreply.github.com>
2023-11-17 22:13:19 +0100

    Add files via upload

---
+++ PROJETPYTHON/Functions.py
@@ -0,0 +1,9 @@
+def login():
+def change_password():
+def afficher_liste_livres():
+def choisir_livres():
+def suspendre_utlisateur():
+def creer_utilisateur():
+def gerer_les_empreints():
+def compter_les_jours():
+
